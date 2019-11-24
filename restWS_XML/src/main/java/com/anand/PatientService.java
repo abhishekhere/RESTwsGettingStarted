19bf21d63f73d2889d9b32fe0571848c17b31fae
AbhishekAnand <AbhishekAnand@192.168.0.106>
2019-11-24 16:41:06 +0530

    First commit.

---
+++ restWS_XML/src/main/java/com/anand/PatientService.java
@@ -0,0 +1,38 @@
+package com.anand;
+
+import java.util.List;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import com.anand.model.Patient;
+
+@Path("/patientservice")
+public interface PatientService {
+	
+	@Path("/patients")
+	@GET
+	public List<Patient> getPatients();
+	
+	@Path("/patients/{identifier}")
+	@GET
+	public Patient getPatient(@PathParam("identifier") long id);
+	
+	@Path("/patients")
+	@POST
+	Response createPatient(Patient patient);
+	
+	@Path("/patients")
+	@PUT
+	Response updatePatient(Patient patient);
+	
+	@Path("/patients/{identifier}")
+	@DELETE
+	Response deletePatient(@PathParam("identifier")long id);
+
+}
